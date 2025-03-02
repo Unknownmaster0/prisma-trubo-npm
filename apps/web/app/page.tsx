d66d2c7a7802c3465898e1679ba07cfc25df68ba
Unknownmaster0 <sagar192002singh@gmail.com>
2025-03-02 16:33:09 +0530

    prisma + turborepo

---
+++ apps/web/app/page.tsx
@@ -0,0 +1,6 @@
+import { prisma } from "@repo/db";
+
+export default async function Home() {
+  const user = await prisma.user.findFirst();
+  return <div>{user?.name ?? "No user added yet"}</div>;
+}
